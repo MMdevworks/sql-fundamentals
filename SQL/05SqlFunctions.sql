3acd88496bf23f48d3ee88bb20acf349700de4cf
Marie Marcos <mmdevq@gmail.com>
2024-04-18 21:00:31 -0700

    sqlfunct upd

--- SQL/05SqlFunctions.sql
+++ SQL/05SqlFunctions.sql
@@ -43,3 +43,27 @@ FROM Sales.Invoices
 SELECT
 ROUND(SUM(LastCostPrice), 0)
 FROM Warehouse.StockItemHoldings
+
+-- Built-in functions that don't require parameter or arguments
+SELECT GETDATE() AS Today;
+SELECT RAND() AS Random;
+
+-- Most functions take at least one parameter or argument
+SELECT TOP 1000 * FROM Warehouse.ColdRoomTemperatures_Archive;
+SELECT
+    MAX(Temperature) AS 'Maximum Temp',
+    MIN(Temperature) AS 'Minimum Temp',
+    AVG(Temperature) AS 'Average Temp'
+FROM Warehouse.ColdRoomTemperatures_Archive;
+
+-- Some functions require multiple parameters or arguments
+SELECT DISTINCT TOP 10
+    InvoiceDate,
+    FORMAT(InvoiceDate, 'd') AS FormattedDate -- standard date formatting for local culture
+FROM Sales.Invoices;
+
+-- Functions can be nested together
+SELECT FORMAT(GETDATE(), 'MMMM dd, yyyy "at" hh:mm');
+
+-- Deterministic VS Non-Deterministic
+-- Deterministic functions always return the same results if given the same input values.
