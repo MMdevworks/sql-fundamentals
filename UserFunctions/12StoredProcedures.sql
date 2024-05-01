de1aae3a8362e7014c9c12038c06f2520592f7ed
Marie Marcos <mmdevq@gmail.com>
2024-04-30 20:37:43 -0700

    stored prcd: parameter

--- UserFunctions/12StoredProcedures.sql
+++ UserFunctions/12StoredProcedures.sql
@@ -3,7 +3,7 @@ GO
 
 -- A stored procedure is prepared SQL code that you can save, so the code can be reused over and over again.
 
--- Basic stored procedure
+-- Ex: Basic stored procedure
 CREATE OR ALTER PROCEDURE Application.uspViewEmployees
 AS
 SELECT PersonID, FullName, IsEmployee, IsSalesperson
@@ -38,4 +38,39 @@ GO
 
 -- Execute multi-task stored procedure
 EXECUTE Application.uspViewData;
+GO
+
+-- Ex: Create a procedure with a parameter
+CREATE OR ALTER PROCEDURE Warehouse.uspInsertColor (@Color AS nvarchar(100))
+AS
+    DECLARE @ColorID INT
+    SET @ColorID = (SELECT MAX(ColorID) FROM Warehouse.Colors)+1;
+    INSERT INTO Warehouse.Colors (ColorID, ColorName, LastEditedBy)
+        VALUES (@ColorID, @Color, 1);
+    SELECT * FROM Warehouse.Colors
+        WHERE ColorID = @ColorID
+        ORDER BY ColorID DESC;
+;
+GO
+
+-- Test the stored procedure
+EXEC Warehouse.uspInsertColor @Color = 'Periwinkle Blue'; -- or remove variable name if supplied in order
+
+SELECT * FROM Warehouse.Colors
+ORDER BY ColorID DESC;
+GO
+
+-- Create another procedure to remove the last color
+CREATE OR ALTER PROCEDURE Warehouse.uspRemoveLastColor
+AS
+    DELETE FROM Warehouse.Colors
+    WHERE ColorID = (SELECT MAX(ColorID) FROM Warehouse.Colors);
+;
+GO
+
+-- Test the row removal
+EXEC Warehouse.uspRemoveLastColor;
+
+SELECT * FROM Warehouse.Colors
+ORDER BY ColorID DESC;
 GO
